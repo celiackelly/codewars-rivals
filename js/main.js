9ed332e613983eff4fd37dc1655b4e5b1d7d85d6
Celia Kelly <celiackelly@gmail.com>
2022-04-18 18:04:24 -0400

    Fix leaderboard to stats update on load/add new

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 //On page load, get usersList from localStorage and parse into array 
 let usersList = JSON.parse(localStorage.getItem('users')) || []
-let usersInfo 
-
-//Map usersList onto URLs for fetch call
-let urls = usersList.map(user => `https://www.codewars.com/api/v1/users/${user}`)
+let usersInfo = []
 
 //Function: Sort array of user stat objects by honor (descending)
 const sortbyHonor = (arr) => arr.sort((a, b) => b.honor - a.honor)
 
 //Function: Fetch updated info/stats for each user; once all fetches have completed, sort the array 
 function getUsersInfo() {
+	let usersList = JSON.parse(localStorage.getItem('users')) || []
+	let urls = usersList.map(user => `https://www.codewars.com/api/v1/users/${user}`)
+
 	Promise.all(urls.map(url => fetch(url)))
 		.then(res => Promise.all(res.map(r => r.json())))
 		.then(data => {
 			usersInfo = sortbyHonor(data)
-			console.log(usersInfo)
+			generateLeaderboard()
 		})
 		.catch(err => console.log(`Error: ${err}`))
 }
@@ -24,34 +24,31 @@ function generateLeaderboard() {
 	const table = document.querySelector('table')
 	const newTBody = document.createElement('tbody')
 	
-		usersInfo.forEach((user, i) => {
-			const tr = document.createElement('tr')
-			newTBody.appendChild(tr)
-		
-			const stats = [i + 1, user.ranks.overall.name, user.username, user.clan, user.honor]
-			console.log(stats)
-			stats.forEach(stat => {
-				const td = document.createElement('td')
-				td.textContent = stat
-				tr.appendChild(td)
-			})
-		console.log(newTBody)
+	usersInfo.forEach((user, i) => {
+		const tr = document.createElement('tr')
+		newTBody.appendChild(tr)
+	
+		const stats = [i + 1, user.ranks.overall.name, user.username, user.clan, user.honor]
+		stats.forEach(stat => {
+			const td = document.createElement('td')
+			td.textContent = stat
+			tr.appendChild(td)
 		})
+	})
 	table.removeChild(document.querySelector('tbody'))
 	table.appendChild(newTBody)
 }
 
-//Function- Add new user to localStorage
-function addUser(e){
+//Function- Add new user to localStorage and run getUserInfo() after fetch
+async function addUser(e){
 	e.preventDefault()
-	
 	const user = document.querySelector('.add-user input').value
 	const url = `https://www.codewars.com/api/v1/users/${user}`
 
-	fetch(url)
+	const newUser = await fetch(url)
 		.then(res => res.json()) // parse response as JSON
 		.then(data => {
-				
+
 			//If no localStorage, set 'users' to empty array 
 			if (!localStorage.getItem('users')) {
 				let usersList= []
@@ -65,30 +62,17 @@ function addUser(e){
 			// Push the new data onto the array
 			usersList.push(data.username);
 
-			// Log the usersList array value
-			console.log(usersList) 
-
 			// Re-serialize the array back into a string and store it in localStorage
 			localStorage.setItem('users', JSON.stringify(usersList));
 		})
 		.catch(err => {
 			console.log(`error ${err}`)
-		});
-}
-
-function regnerateLeaderboardOnAdd() {
-	addUser()
-	getUpdatedStats()
-	generateLeaderboard()
+		})
+	getUsersInfo()
 }
 
 //On page load, get users stats and generate leaderboard
 getUsersInfo()
-generateLeaderboard()
 
-//Listen for add-user form submission
-document.querySelector('.add-user').addEventListener('submit', regenerateLeaderboard)
-//how do I then fire getUpdatedStats() and generateLeaderboard() ? maybe just combine those three in one larger function tied to the event listener
-
-
-
+// //Listen for add-user form submission
+document.querySelector('.add-user').addEventListener('submit', addUser)
