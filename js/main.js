cb23f48aa2264f4feca929ee0db0612cf31a7b0c
Celia Kelly <81911167+celiackelly@users.noreply.github.com>
2022-04-18 12:56:13 -0400

    Fix submit event with e.preventDefault()

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 //On page load, get usersList from localStorage and parse into array 
-// let usersList = JSON.parse(localStorage.getItem('users')) || []
-let usersList = ['sashavining', 'celiackelly', 'xRichard']
+let usersList = JSON.parse(localStorage.getItem('users')) || []
 let usersInfo 
 
 //Map usersList onto URLs for fetch call
@@ -23,40 +22,12 @@ function getUsersStats() {
 //On page load, get users stats 
 getUsersStats()
 
+//Listen for add-user form submission
+document.querySelector('.add-user').addEventListener('submit', addUser)
 
-
-const table = document.querySelector('table')
-const newTBody = document.createElement('tbody')
-
-//Generate leaderboard table
-	// usersInfo.forEach((user, i) => {
-	// 	console.log('hello')
-	
-	// 	const tr = document.createElement('tr')
-	// 	newTBody.appendChild(tr)
-		
-	// 	const stats = [i + 1, user.ranks.overall.name, user.username, user.clan, user.honor]
-	// 	console.log(stats)
-	// 	stats.forEach(stat => {
-	// 		const td = document.createElement('td')
-	// 		td.textContent = stat
-	// 		tr.appendChild(td)
-	// 	})
-	// 	console.log(newTBody)
-	// })
-
-
-
-// For each user in usersInfo, add stats to the DOM
-
-// table.removeChild(document.querySelector('tbody'))
-// table.appendChild(newTBody)
-// console.log(newTBody)
-
-
-document.querySelector('.add-user button').addEventListener('click', addUser)
-
-function addUser(){
+//Function- Add new user to localStorage
+function addUser(e){
+	e.preventDefault()
 	
 	const user = document.querySelector('.add-user input').value
 	const url = `https://www.codewars.com/api/v1/users/${user}`
@@ -64,7 +35,6 @@ function addUser(){
 	fetch(url)
 		.then(res => res.json()) // parse response as JSON
 		.then(data => {
-			console.log(data)
 				
 			//If no localStorage, set 'users' to empty array 
 			if (!localStorage.getItem('users')) {
@@ -79,12 +49,11 @@ function addUser(){
 			// Push the new data onto the array
 			usersList.push(data.username);
 
-			// Log the array value
+			// Log the usersList array value
 			console.log(usersList) 
 
 			// Re-serialize the array back into a string and store it in localStorage
 			localStorage.setItem('users', JSON.stringify(usersList));
-
 		})
 		.catch(err => {
 				console.log(`error ${err}`)
@@ -92,27 +61,30 @@ function addUser(){
 }
 
 
-		// 	//Add user data to the DOM in the leaderboard
-		// 	usersArray.forEach((user, i) => {
+const table = document.querySelector('table')
+const newTBody = document.createElement('tbody')
 
-		// 		const tr = document.createElement('tr')
-		// 		newTBody.appendChild(tr)
+//Generate leaderboard table
+	// usersInfo.forEach((user, i) => {
+	
+	// 	const tr = document.createElement('tr')
+	// 	newTBody.appendChild(tr)
+		
+	// 	const stats = [i + 1, user.ranks.overall.name, user.username, user.clan, user.honor]
+	// 	console.log(stats)
+	// 	stats.forEach(stat => {
+	// 		const td = document.createElement('td')
+	// 		td.textContent = stat
+	// 		tr.appendChild(td)
+	// 	})
+	// 	console.log(newTBody)
+	// })
 
-		// 		const userInfo = [i + 1, user.ranks.overall.name, user.username, user.clan, user.honor]
+// For each user in usersInfo, add stats to the DOM
 
-		// 		userInfo.forEach(info => {
-		// 			const td = document.createElement('td')
-		// 			td.textContent = info
-		// 			tr.appendChild(td)
-		// 		})
-
-		// 		newTBody.appendChild(tr)
-		// 	})
-
-		// 	table.removeChild(document.querySelector('tbody'))
-		// 	table.appendChild(newTBody)
-
-		// })
+// table.removeChild(document.querySelector('tbody'))
+// table.appendChild(newTBody)
+// console.log(newTBody)
 
 //PROBLEM - the honor, rank, and position/sorting won't update, because those values are coming from local storage! 
 //We need to fetch the data for each user in local storage each time
@@ -120,7 +92,9 @@ function addUser(){
 //Also, there must be a better solution to removing and replacing the whole tbody each time...
 
 
-//Problem - DOM stuff needs to be INSIDE the fetch call; otherwise, the rest of the document execution proceeds while waiting for the fetch to finish. BUT if all the DOM stuff happens inside the fetch call, you can't sort the leaderboard, because the fetch is being executed one username at a time. Might be able to sort the table once in place w/a separate function? 
+//Problem - DOM stuff needs to be INSIDE the fetch call; otherwise, the rest of the document execution proceeds while waiting for the fetch to finish. 
+//BUT if all the DOM stuff happens inside the fetch call, you can't sort the leaderboard, because the fetch is being executed one username at a time. 
+//Might be able to sort the table once in place w/a separate function? 
 
 
 
